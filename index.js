0ef948de831b439f274900e61f46b53578957bc6
Justin Mathew <justinmathew@Justins-Air.fritz.box>
2022-08-23 00:58:35 +0200

    adding database

--- index.js
+++ index.js
@@ -1,6 +1,12 @@
-
 const express = require('express');
-
+const Pool = require('pg').Pool;
+const pool = new Pool({
+    user:'postgres',
+    host:'localhost',
+    database:'firstdb',
+    password:'password',
+    port: 5432,
+});
 const app = express();
 
 app.use(express.json());
@@ -11,11 +17,20 @@ app.use(express.urlencoded({
 app.get('/',(req,res)=>{
     res.sendFile(__dirname + '/form.html')
 });
-app.post('/',(req,res)=>{
-console.log(req.body.name);
-console.log(req.body.email);
-
-res.sendStatus(200);
+app.post('/', (req,res) =>{
+    const {name, email} = req.body;
+    pool.query('INSERT INTO firsttab (name,email) VALUES($1,$2)',[name,email],(error,results)=>{
+    if (error){
+        console.log(error);
+    }
+    else{
+        res.status(201).send(`User added `);
+    }
 });
-app.listen(3000);
-console.log("server listening on port 3000");
+});
+app.listen(3000,()=>
+    console.log("server listening on port 3000"));
+
+
+
+
